23bb00ae84013f14ab0f44090ca11d54e53fd79d
Adrian Luca Lemon <45937542+eidriahn@users.noreply.github.com>
2024-06-03 22:23:44 +0300

    change to adrian credits

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ export const Footer = () => {
   return (
     <div className="w-full text-slate-50 p-5 border-t-[0.5px] border-t-white">
       Built by{" "}
-      <a href="https://en.lemon.be/" target="_blank" className="underline">
-      Lemon
+      <a href="https://adrianluca.dev" target="_blank" className="underline">
+      Adrian Luca
       </a>
     </div>
   );
